30f5c3e2312f71452a7da9ba1a81743eb20e9f96
CHO1YJ <c5898559631@gmail.com>
2022-05-08 01:50:53 +0900

    Completed adding explanation of code

--- class_220504.py
+++ class_220504.py
@@ -16,25 +16,27 @@ for n in range(len(input_data)):
     sorted_input_data[n, :] = input_data[n, :]
 sorted_input_data = sorted_input_data[sorted_input_data[:, 0].argsort()] 
 
-sorted_in_x0 = sorted_input_data[:, 0] # x0를 오름차순으로 정렬
-sorted_in_x1 = sorted_input_data[:, 1] # x1을 오름차순으로 정렬
-y_real = sorted_input_data[:, 2] # 실제값 배열 정의
+# sorted_in_x0 = sorted_input_data[:, 0] # x0를 오름차순으로 정렬
+# sorted_in_x1 = sorted_input_data[:, 1] # x1을 오름차순으로 정렬
+# y_real = sorted_input_data[:, 2] # 실제값 배열 정의
     
-# Setting Variable
-in_x0 = input_data[:, 0] # 입력; x0
-in_x1 = input_data[:, 1] # 입력; x1
-out_y = input_data[:, 2] # 출력; y
+# # Setting Variable
+# in_x0 = input_data[:, 0] # 입력; x0
+# in_x1 = input_data[:, 1] # 입력; x1
+# out_y = input_data[:, 2] # 출력; y
 
+# 데이터를 y에 대한 값에 따라 분류
 list_color = []
-input_data_0 = np.zeros((int(len(input_data) / 2), 3))
-input_data_1 = np.zeros((int(len(input_data) / 2), 3))
+input_data_0 = np.zeros((int(len(input_data) / 2), 3)) # 출력이 0인 데이터 기록함
+input_data_1 = np.zeros((int(len(input_data) / 2), 3)) # 출력이 1인 데이터 기록함
+# 기존 데이터에서 250을 기준으로 0과 1로 나뉘므로 이를 통해 index를 조정하여 초기화
 for n in range(len(input_data)):
     if n < len(input_data) / 2:
         list_color.append('r')
-        input_data_0[n, :] = input_data[n, :]
+        input_data_0[n, :] = input_data[n, :] # 250 이하의 y가 0인 데이터
     elif n >= len(input_data) / 2:
         list_color.append('b')
-        input_data_1[n - 250, :] = input_data[n, :]
+        input_data_1[n - 250, :] = input_data[n, :] # 250 이상의 y가 1인 데이터
         
 in_x0_0 = input_data_0[:, 0] # 입력; x0
 in_x1_0 = input_data_0[:, 1] # 입력; x1
@@ -61,36 +63,39 @@ history_w0 = [] # weight0 기록함
 history_w1 = [] # weight1 기록함
 history_w2 = [] # weight1 기록함
 
-history_p = []
-history_y_hat = []
+history_p = [] # 확률 probability 기록함
+history_y_hat = [] # y_hat 기록함
 
 history_w0.append(np.random.rand() * 2 + 3) # weight0 초기값 초기화
 history_w1.append(np.random.rand() * 2 + 4) # weight1 초기값 초기화
 history_w2.append(np.random.rand() * 2 - 6) # weight1 초기값 초기화
 # 함수의 입력 초기값
-weight_w0 = history_w0[0]
-weight_w1 = history_w1[0]
-weight_w2 = history_w2[0]
+weight_w0 = history_w0[0] # w0 초기값
+weight_w1 = history_w1[0] # w1 초기값
+weight_w2 = history_w2[0] # w2 초기값
 learning_rate = 0.015# a; alpha / 학습률
-step = 1000
+step = 1000 # 학습 횟수
 
-def Func_Logistic_Regression(datax0, datax1, y_r, w0, w1, w2, a): # 로지스틱 회귀 함수 정의
-    p = p_init
+# Logistic Regression 함수 구현
+# x1, x0 y_real 데이터와 가중치 w0, w1, w2 그리고 학습률 a가 함수의 입력
+def Func_Logistic_Regression(datax0, datax1, y_r, w0, w1, w2, a):
+    p = p_init # 확률의 초기값 설정
+    # 학습 횟수에 따라 w0, w1, w2 값 갱신
     for t in range(step):       
         w0 = w0 - a * np.sum((p - y_r) * datax0) / len(datax0)
         w1 = w1 - a * np.sum((p - y_r) * datax1) / len(datax0)
         w2 = w2 - a * np.sum(p - y_r) / len(datax0)
         
+        # 갱신된 w0, w1, w2에 따른 확률값 p 갱신
         p = 1 / (1 + np.exp(-(w0 * datax0 + w1 * datax1 + w2)))
             
+        # 데이터 확인 및 추후 도식화와 함수에 사용하기 위한 데이터 수집
         history_y_hat.append(y_hat)
         history_p.append(p)
-        
         history_w0.append(w0)
         history_w1.append(w1)
         history_w2.append(w2)
            
-    # 출력할 값들을 return
     return True
 
 # (2)
@@ -144,8 +149,10 @@ def Dist_Set(data): # 원본 데이터를 입력값으로 함
     
     return train_set, val_set, test_set # 분할된 DB 반환
 
+# 훈련, 검증, 평가 DB 초기화; 입력은 정렬된 초기 데이터
 training_set, validation_set, test_set = Dist_Set(sorted_input_data)
 
+# 랜덤으로 추출된 훈련, 평가 DB를 다시 정렬
 training_set = training_set[training_set[:, 0].argsort()] 
 test_set = test_set[test_set[:, 0].argsort()] 
 
@@ -154,6 +161,7 @@ sorted_train_x0 = training_set[:, 0]
 sorted_train_x1 = training_set[:, 1]
 sorted_train_y = training_set[:, 2]
 
+# 정렬된 데이터의 출력 y를 0과 1로 구분하여 기록
 container_train_x0_0 = []
 container_train_x1_0 = []
 container_train_x0_1 = []
@@ -171,6 +179,7 @@ sorted_test_x0 = test_set[:, 0]
 sorted_test_x1 = test_set[:, 1]
 sorted_test_y = test_set[:, 2]
 
+# 정렬된 데이터의 출력 y를 0과 1로 구분하여 기록
 container_test_x0_0 = []
 container_test_x1_0 = []
 container_test_x0_1 = []
@@ -198,12 +207,17 @@ plt.grid(True, alpha=0.5)
 plt.show()
 
 # (3) 정확도 및 CEE
+# y_hat의 초기값 초기화
 y_hat = weight_w0 * sorted_train_x0 + weight_w1 * sorted_train_x1 + weight_w2
+# 확률의 초기값 초기화
 p_init = 1 / (1 + np.exp(-y_hat))
+# Logistic Regression 함수 호출
 Func_Logistic_Regression(sorted_train_x0, sorted_train_x1, sorted_train_y, \
                          weight_w0, weight_w1, weight_w2, learning_rate)
 
+# y_hat 값 기록함 정의
 decided_y_hat = np.zeros((step, len(sorted_train_x0)))
+# y_hat을 확률 값에 따라 1과 0으로 구분하는 함수 정의; 입력값은 확률값 p
 def Decide_y_hat(p):
     for n in range(step):
         for m in range(len(sorted_train_x0)):
@@ -213,36 +227,50 @@ def Decide_y_hat(p):
                 decided_y_hat[n][m] = 0
     return True
 
+# y_hat 결정 함수 호출
 Decide_y_hat(history_p)
 
+# 정확도 기록함 행렬 정의; 각각의 성분에 대하여 True와 False로 표현
 matrix_accuracy = np.zeros((step, len(sorted_train_x0)))
+# 정확도 값 정의
 accuracy = 0.
+# 정확도 기록함 정의; 학습 횟수에 따라 확률로 표현
 history_accuracy = []
+# 정확도 측정 함수 정의; 입력은 결정된 y_hat 값과 훈련 DB의 y(0과 1로 구성)값
 def Measure_Accuracy(dcd_y_hat, train_y):
     for n in range(step):
         for m in range(len(sorted_train_x0)):
+            # 결정된 y_hat과 훈련 DB의 y값이 같으면 True 아니면 False로 초기화
             if dcd_y_hat[n][m] == train_y[m]:
                 matrix_accuracy[n][m] = True
             else:
                 matrix_accuracy[n][m] = False
+    # 정확도의 정도에 대한 카운트 정의
     count_accuracy = 0
     for n in range(step):
         for m in range(len(sorted_train_x0)):
+            # True는 곧 1이므로 1이 많으면 정확도가 높은 것!
             if matrix_accuracy[n][m] == 1:
                 count_accuracy = count_accuracy + 1
+        # 정확도 초기화 및 기록
         accuracy = (count_accuracy / 350) * 100
         history_accuracy.append(accuracy)
+        # 학습횟수마다 count를 해야하므로 0으로 초기화 후 다시 count
         count_accuracy = 0
     return True
 
+# 정확도 측정 함수 호출
 Measure_Accuracy(decided_y_hat, sorted_train_y)
+# CEE 기록함 정의
 history_CEE = []
+# CEE 측정 함수 정의; 입력은 확률값 p와 훈련 DB의 y값
 def Measure_CEE(p, train_y):
     for n in range(step):
         CEE = -np.sum(train_y * np.log(p[n]) + (1 - train_y) * np.log(1 - p[n])) / len(train_y)
         history_CEE.append(CEE)
     return True
 
+# CEE 측정 함수 호출
 Measure_CEE(history_p, sorted_train_y)
 
 # Setting step
