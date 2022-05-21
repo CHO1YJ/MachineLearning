1c7be0a6761e3345fda6fec241509e077275fb25
CHO1YJ <c5898559631@gmail.com>
2022-05-21 12:23:53 +0900

    Started coding Hidden Layer calculates

--- class_220518.py
+++ class_220518.py
@@ -8,6 +8,9 @@ import pandas as pd
 input_data = pd.read_csv('Data_Base/NN_data.csv', \
                          index_col=0).to_numpy(dtype='float')
     
+temp_input_data = input_data[:, 0:3]
+
+# (1) One-Hot Encoding 구현
 def One_Hot_Encoding(data):
     Encoding_y = []
     data_y = data[:, 3]
@@ -39,15 +42,30 @@ y_One_Hot_Encoding = One_Hot_Encoding(input_data)
 coverted_data = np.concatenate([np.delete(input_data, 3, axis=1) , \
                                 y_One_Hot_Encoding], 1)
     
+# (2) Two-Layer Neural Network 구현
+# Setting Variable
+num_hidden_layer = 3 # Hidden layer의 Node 수
+# history_a = [] # Hidden layer의 y_hat
+history_b =[] # Hidden layer의 출력
+
+def Hidden_Layer(num_l):
+    a = np.zeros((num_l, 1))
+    b = np.ones((num_l + 1, 1))
+    list_v = []
+    for n in range(len(input_data) + 1):
+        v = np.random.rand() * 2 + 3
+        list_v.append(v)
+    for l in range(num_l):
+        for m in range(len(input_data)):
+            a[l] = np.sum(list_v[m] * temp_input_data[m]) + list_v[900] * 1
+            b[l] = 1 / (1 + np.exp(-a[l]))
+    # for l in range(num_l):
+    #     for m in range(len(input_data)):
+    #         a[l] = np.sum(list_v[m] * input_data[m, :])
+    #         b[l] = 1 / (1 + np.exp(-a[l]))
+    return a, b, list_v
     
-    
-    
-    
-    
-    
-    
-    
-    
+a_y_hat, b_output, v_list = Hidden_Layer(num_hidden_layer)
     
     
     
