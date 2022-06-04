54097f5012f598167844d4c86cad89a35519b117
CHO1YJ <c5898559631@gmail.com>
2022-06-04 22:09:20 +0900

    Be modifying to^Cenew weights of a ANN.py file

--- ANN.py
+++ ANN.py
@@ -61,10 +61,10 @@ def Two_Layer_Neural_Network(data, num_l, num_q):
     for n in range(data.shape[1]):
         for l in range(num_l):
             # 가우시안 함수에 따라 랜덤하게 가중치 값 초기화
-            list_v[n][l] = np.random.randn()
+            list_v[n][l] = np.random.randn() * 10
     alpha = data.dot(list_v) # Hidden Layer의 입력 초기화
     b = 1 / (1 + np.exp(-alpha)) # Hidden Layer의 출력 초기화
-    b = np.concatenate((b , np.ones((900, 1))), axis=1) # bias 첨부
+    b = np.concatenate((b , np.ones((len(data), 1))), axis=1) # bias 첨부
           
     # Output Layer
     # Output Layer의 속성 수는 출력 class의 수
@@ -75,7 +75,7 @@ def Two_Layer_Neural_Network(data, num_l, num_q):
     list_w = np.zeros((num_l + 1, Q))
     for l in range(num_l + 1):
         for q in range(Q):
-            list_w[l][q] = np.random.randn()
+            list_w[l][q] = np.random.randn() * 10
     
     beta = b.dot(list_w) # Output Layer의 입력 초기화
     y_hat = 1 / (1 + np.exp(-beta)) # Output Layer의 출력 초기화
@@ -94,12 +94,13 @@ def Decide_y_hat(p, data):
                 decided_y_hat[n][m] = 0
     return True
 
-# 정확도 기록함 행렬 정의; 각각의 성분에 대하여 True와 False로 표현
-matrix_accuracy = np.zeros((len(y_One_Hot_Encoding), 1))
+
 # 정확도 값 정의
 accuracy = 0.
 # 정확도 측정 함수 정의; 입력은 결정된 y_hat 값과 훈련 DB의 y(0과 1로 구성)값
 def Measure_Accuracy(dcd_y_hat, data):
+    # 정확도 기록함 행렬 정의; 각각의 성분에 대하여 True와 False로 표현
+    matrix_accuracy = np.zeros((len(data), 1))
     for m in range(len(data)):
         # 결정된 y_hat과 훈련 DB의 y값이 같으면 True 아니면 False로 초기화
         if dcd_y_hat[m].tolist() == data[m].tolist():
@@ -173,59 +174,100 @@ def Dist_Set(data): # 원본 데이터를 입력값으로 함
 # 함수 호출 및 반환값에 대한 학습, 검증, 평가 DB 초기화
 training_set, validation_set, test_set = Dist_Set(input_data_added_bias)
 
-# (5) 신경망 학습 함수 및 가중치 갱신
-# 신경망 학습 함수
-learning_rate = 0.015
-def learning_ANN(data, v, w, y_hat, y_real, b, num_l, num_q):
+# (5)
+def shuffle_data(data):
     np.random.shuffle(data)
+    OHE_y = np.zeros((len(data), 3))
+    for m in range(data.shape[0]):
+        for n in range(len(input_data_added_bias)):
+            if data[m].tolist() == input_data_added_bias[n].tolist():
+                OHE_y[m] = y_One_Hot_Encoding[n]
+    return OHE_y
+
+# (6) 신경망 학습 함수 및 가중치 갱신
+# 신경망 학습 함수
+learning_rate = 0.001
+def learning_ANN(data, v, w, in_y_hat, y_real, in_b, num_l, num_q):
     u = learning_rate
     
     renewal_v = np.zeros((data.shape[1], num_l))
+    dmse_vml = np.zeros((data.shape[1], num_l))
     renewal_w = np.zeros((num_l + 1, num_q))
+    dmse_wlq = np.zeros((num_l + 1, num_q))
     
-    list_dmse_vml = []
-    dmse_vml = np.zeros((data.shape[1]))
-    for n in range(data.shape[1]):
-        for l in range(num_l):
-            dmse_vml = 2 * np.sum((y_hat - y_real) * y_hat * (1 - y_hat) * w[l])  \
-                                * b[n][l] * (1 - b[n][l])
-            dmse_vml = dmse_vml * data[n]
-            list_dmse_vml.append(dmse_vml)
-            renewal_v[:, l] = v[:, l] - u * dmse_vml
-            
-    # for n in range(data.shape[1]):
+    list_mem2 = []
+    list_mem1 = []
+    for n in range(data.shape[0]):
+        for m in range(data.shape[1]):
+            for l in range(num_l):
+                for q in range(num_q):
+                    dmse_vml[m][l] = 2 * np.sum((in_y_hat[n][q] - y_real[n][q]) \
+                            * in_y_hat[n][q] * (1 - in_y_hat[n][q]) * w[l][q]) \
+                            * in_b[n][l] * (1 - in_b[n][l]) * data[n][m]
+        list_mem1.append(dmse_vml)
+        renewal_v = v - u * dmse_vml
+    
+        for l in range(num_l + 1):
+            for q in range(num_q):
+                dmse_wlq[l][q] = 2 * (in_y_hat[n][q] - y_real[n][q]) \
+                                  * in_y_hat[n][q] * (1 - in_y_hat[n][q]) * in_b[n][l]
+        list_mem2.append(dmse_wlq)
+        renewal_w = w - u * dmse_wlq
+        
+        # for l in range(num_l):
+        #     for m in range(data.shape[1]):
+        #         dmse_vml[m][l] = 2 * np.sum(((in_y_hat[n] - y_real[n]) \
+        #                 * in_y_hat[n] * (1 - in_y_hat[n])).dot(np.transpose(w))) \
+        #                 * in_b[n][l] * (1 - in_b[n][l]) * data[n][m]
+        # # dmse_vml = np.reshape(dmse_vml, (data.shape[1], 1))
+        # list_mem1.append(dmse_vml)
+        # renewal_v = v - u * dmse_vml
+    
+    # # 가중치 v 갱신; for문 중복
+    # # m하고 l하고 q
+    # # for n in range(~~)
+    # dmse_vml = np.zeros(data.shape[1])
+    # for n in range(data.shape[0]):
     #     for l in range(num_l):
-    #         renewal_v[n][l] = v[n][l] - u * mse_vml
-    
-    
-    # # 가중치 v는 입력과 Hidden Layer의 node 수에 따라 size가 결정
-    # list_v = np.zeros((data.shape[1], num_l))
-    # for n in range(data.shape[1]):
-    #     for l in range(num_l):
-    #         # 가우시안 함수에 따라 랜덤하게 가중치 값 초기화
-    #         list_v[n][l] = np.random.randn()
-    # alpha = data.dot(list_v) # Hidden Layer의 입력 초기화
-    # b = 1 / (1 + np.exp(-alpha)) # Hidden Layer의 출력 초기화
-    # b = np.concatenate((b , np.ones((900, 1))), axis=1) # bias 첨부
-    
-    # # 가중치 w는 Hidden Layer와 출력의 node 수에 따라 size가 결정
-    # list_w = np.zeros((num_l + 1, Q))
+    #         list_mem1 = []
+    #         for m in range(data.shape[1]):
+    #             dmse_vml = 2 * np.sum((y_hat - y_real) * y_hat * (1 - y_hat) * w[l])  \
+    #                             * b[n][l] * (1 - b[n][l]) * data[m]
+    #             list_mem1.append(dmse_vml)
+    #             dmse_vml = np.reshape(dmse_vml, (data.shape[1], 1))
+    #             renewal_v = v - u * dmse_vml
+    #             # renewal_v[:, l] = v[:, l] - u * dmse_vml
+    # # 가중치 w 갱신
+    # dmse_wlq = np.zeros(num_q)
     # for l in range(num_l + 1):
-    #     for q in range(Q):
-    #         list_w[l][q] = np.random.randn()
+    #     list_mem2 = []
+    #     for q in range(num_q):
+    #         dmse_wlq = 2 * (y_hat[q] - y_real[q]) * y_hat[q] * (1 - y_hat[q]) * b[l]
+    #         list_mem2.append(dmse_wlq)
+    #         renewal_w[:, q] = w[:, q] - u * dmse_wlq
     
-    # beta = b.dot(list_w) # Output Layer의 입력 초기화
-    # y_hat = 1 / (1 + np.exp(-beta)) # Output Layer의 출력 초기화
+    # 갱신된 v, w를 통한 y_hat 계산
+    alpha = data.dot(renewal_v) # Hidden Layer의 입력 초기화
+    renewal_b = 1 / (1 + np.exp(-alpha)) # Hidden Layer의 출력 초기화
+    renewal_b = np.concatenate((renewal_b , np.ones((len(data), 1))), axis=1) # bias 첨부
     
-    # # 가중치 v와 w 그리고 Hidden, Output Layer의 출력을 반환
-    return renewal_v, list_dmse_vml
+    beta = renewal_b.dot(renewal_w) # Output Layer의 입력 초기화
+    renewal_y_hat = 1 / (1 + np.exp(-beta)) # Output Layer의 출력 초기화
+    
+    # 가중치 v와 w 그리고 b, y_hat, y_real의 출력을 반환
+    return renewal_v, renewal_w, renewal_b, renewal_y_hat, list_mem1, list_mem2
 
 # 가중치 갱신
 train_data = training_set
+# Shuffle data
 epoch = 10
 initial_v, b_output_hidden_layer, initial_w, y_hat = \
-    Two_Layer_Neural_Network(input_data_added_bias, num_hidden_layer, num_output_layer)
-for n in range(10):
+    Two_Layer_Neural_Network(train_data, num_hidden_layer, num_output_layer)
+weight_v = initial_v
+weight_w = initial_w
+b = b_output_hidden_layer
+sorted_OHE_y = shuffle_data(train_data)
+for n in range(epoch):
     print(n)
     # y_hat 값 기록함 정의
     decided_y_hat = np.zeros((y_hat.shape[0], y_hat.shape[1]))
@@ -233,12 +275,11 @@ for n in range(10):
     Decide_y_hat(y_hat, y_hat)
         
     # 정확도 측정 함수 호출
-    accuracy = Measure_Accuracy(decided_y_hat, y_One_Hot_Encoding)
+    accuracy = Measure_Accuracy(decided_y_hat, sorted_OHE_y)
     print("정확도 : ", accuracy)
-            
-    weight_v, dmse_vml = learning_ANN(train_data, initial_v, initial_w, y_hat, y_One_Hot_Encoding, \
-                    b_output_hidden_layer, num_hidden_layer, num_output_layer)
-        
+           
+    weight_v, weight_w, b, y_hat, mem1, mem2 = learning_ANN(train_data, weight_v, weight_w, y_hat, sorted_OHE_y, \
+                    b, num_hidden_layer, num_output_layer)
     # weight_v, weight_w, learned_y_hat = learning_ANN(train_data, \
     #             initial_v, initial_w, y_hat, y_One_Hot_Encoding, \
     #                 num_hidden_layer, num_output_layer)
